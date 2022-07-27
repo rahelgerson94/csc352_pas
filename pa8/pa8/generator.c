53a32cbd13db6ee8af9b5fd8fc602823e1204215
rahelgerson94 <rahelgerson94@gmail.com>
2022-07-27 11:08:02 -0700

    valgrind tests in generator.c

--- pa8/pa8/generator.c
+++ pa8/pa8/generator.c
@@ -55,9 +55,9 @@ int main(){
 #endif
     
 #ifdef test_pyramid_valgrind
-    Scene3D* pyramid_s = Scene3D_create();
-    Object3D* pyramid_o0 = Object3D_create_pyramid(o, 6, 5, "up");
-    Scene3D_append(pyramid_s, pyramid_o0);
+    Scene3D* pyramid_s2 = Scene3D_create();
+    Object3D* pyramid_o32 = Object3D_create_pyramid(o, 6, 5, "up");
+    Scene3D_append(pyramid_s2, pyramid_o0);
     Scene3D_write_stl_text(cuboid, "out.stl");
     free(pyramid_s);
     
